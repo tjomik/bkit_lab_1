0d91d4dbf20a444ed6367a9dd25a274d75b8f187
TJOMA\Artjoms <tjomik1996@gmail.com>
2017-10-11 17:59:23 +0300

    Заливаем первую версию

---
+++ LAB_1/LAB_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            a = Checking("A");
+            b = Checking("B");
+            c = Checking("C");
+            QuadraticEquation equation = new QuadraticEquation(a, b, c);
+            equation.Answer();
+            Console.ReadKey();
+
+
+        }
+       static double Checking(string str1)
+        {
+            double coef = 0;
+            string str;
+            int i = 0;
+            while (i < 1)
+            {
+                Console.WriteLine("Введите "+ str1+": ");
+                str = Console.ReadLine();
+
+                if (double.TryParse(str, out coef) == true)
+                {
+                    coef = Convert.ToDouble(str);
+                    i++;
+                }
+
+            }
+            return coef;
+        }
+      
+    }
+}
---
+++ LAB_1/LAB_1/QuadraticEquation.cs
@@ -0,0 +1,48 @@
+﻿//using Microsoft.Analytics.Interfaces;
+//using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LAB_1
+{
+    class QuadraticEquation
+    {
+        double a, b, c, d, x1, x2;
+        public QuadraticEquation(double a1, double b1, double c1)
+        {
+            a = a1;
+            b = b1;
+            c = c1;
+        }
+        double Discriminant()
+        {
+            d = b * b - 4 * a * c;
+            return d;
+        }
+        public void Answer()
+        {
+            Discriminant();
+            if (d == 0)
+            {
+                x1 = -b / (2 * a);
+                x2 = x1;
+                Console.WriteLine("D=" + d + "\n"+"X1=X2=" + x1);
+            }
+            else if (d < 0)
+            {
+                Console.WriteLine("D=" + d + "\n"+"Ответ является комплексным числом");
+            }
+            else
+            {
+                x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                Console.WriteLine("D="+d+"\n"+"X1="+x1+"\n"+"X2="+x2);
+            }
+
+        }
+
+    }
+}
